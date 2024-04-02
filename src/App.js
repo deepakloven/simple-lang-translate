2a891f1e403b813bd61bbdff5317337afa723db4
dt3280097@gmail.com <dt3280097@gmail.com>
2024-04-02 07:15:00 +0545

    language translate first commmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './components/Data.json';
 
 function App() {
+  const [selectedLanguageData, setSelectedLanguageData] = useState([]);
+
+  const handleClick = (lang) => {
+    const filteredData = data.filter(item => item.hasOwnProperty(lang));
+    setSelectedLanguageData(filteredData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <button className='button bt1' onClick={() => handleClick('np')}>Nepali</button>
+      <button className='button bt2' onClick={() => handleClick('ch')}>Chinese</button>
+      <button className='button bt3' onClick={() => handleClick('en')}>English</button>
+
+      {/* Render filtered data based on selected language */}
+      {selectedLanguageData.map((item, i) => (
+        <div className='border m-3' key={i}>
+          {/* Render content based on selected language */}
+          <h1>{item.np || item.ch || item.en}</h1>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import languageData from '../components/Data.json'; 
+import '../css/navbar.css';
+
+function Navbar() {
+  const [selectedLanguage, setSelectedLanguage] = useState('en');
+
+  const handleLanguageChange = (lang) => {
+    setSelectedLanguage(lang);
+  };
+
+  return (
+    <>
+      <div>
+        <nav className="navbar">
+          <div className="navbar-logo">Logo</div>
+          <ul className="navbar-menu">
+            <li className="navbar-item">Home</li>
+            <li className="navbar-item dropdown">
+              <span className="dropdown-toggle">Dropdown</span>
+              <ul className="dropdown-menu">
+                <li className="dropdown-item" onClick={() => handleLanguageChange('np')}> Nepali</li>
+                <li className="dropdown-item" onClick={() => handleLanguageChange('ch')}>Chinese</li>
+                <li className="dropdown-item" onClick={() => handleLanguageChange('en')}>English</li>
+              </ul>
+            </li>
+            <li className="navbar-item">About</li>
+            <li className="navbar-item">Contact</li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <div className='first-container'>
+          <div className='left-container'>
+          <h1>{languageData[0][selectedLanguage].greeting}</h1>
+            <span>{languageData[0][selectedLanguage].name}</span>
+            <p>{languageData[0][selectedLanguage].profession}</p>
+            </div>
+            <div className='right-container'>
+            </div>
+
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
